4a13d5af9826c0b080459c268ebd8d6a3767c8ea
purirahul <rpuri648@gmail.com>
2022-01-16 19:46:08 +0530

    Uses View, Text, TextInput and State

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { Button, StyleSheet, Text, View, TextInput } from "react-native";
 
 export default function App() {
+	const [name, setName] = useState("");
+
+	const onUpdateNamepressHandler = (value) => {
+		setName("Rahul Puri");
+	};
+
 	return (
 		<View style={styles.container}>
-			<Text>Welcome to my first React Native Project !</Text>
-			<StatusBar style="auto" />
+			{name !== "" && (
+				<View style={styles.header}>
+					<Text style={styles.textStyle}>Hello, {name}!</Text>
+				</View>
+			)}
+			<View style={styles.inputContianer}>
+				<Text>Enter your name: </Text>
+				<TextInput
+					style={styles.inputStyle}
+					placeholder="Your name goes here"
+					onChangeText={(val) => setName(val)}
+				/>
+			</View>
+			<View style={styles.buttonStyle}>
+				<Button
+					title="Update Name"
+					onPress={onUpdateNamepressHandler}
+				/>
+			</View>
 		</View>
 	);
 }
@@ -16,5 +39,27 @@ const styles = StyleSheet.create({
 		backgroundColor: "#fff",
 		alignItems: "center",
 		justifyContent: "center",
+	},
+	header: {
+		backgroundColor: "#87ceeb",
+		padding: 20,
+	},
+	textStyle: {
+		fontWeight: "bold",
+	},
+	buttonStyle: {
+		marginTop: 20,
+	},
+	inputContianer: {
+		marginTop: 20,
+		backgroundColor: "orange",
+		padding: 20,
+	},
+	inputStyle: {
+		borderWidth: 1,
+		borderColor: "#777",
+		margin: 10,
+		padding: 10,
+		width: 200,
 	},
 });
